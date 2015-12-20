3a058378f372fb115a777c4bdbe4b5d9ff222eec
admin <admin@admindemacbook-pro.local>
2015-12-20 18:50:11 +0800

    Initial Commit

---
+++ 生成二维码/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  生成二维码
+//
+//  Created by admin on 15/12/20.
+//  Copyright © 2015年 AlezJi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 生成二维码/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  生成二维码
+//
+//  Created by admin on 15/12/20.
+//  Copyright © 2015年 AlezJi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
